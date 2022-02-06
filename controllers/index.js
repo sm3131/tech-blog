f8b4060ca5906b3c0416d099843560f446fa3663
sm3131 <sm2683@nau.edu>
2022-02-05 20:28:04 -0700

    created routes to display user dashboard with their posts and a form to add a new post

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
-//const dashboardRoutes = require('./dashboard-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
 router.use('/', homeRoutes);
 
-//router.use('/dashboard', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
