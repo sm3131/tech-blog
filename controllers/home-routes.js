5c03a850d61d2dcdc49819ee455344c7a0a4494b
sm3131 <sm2683@nau.edu>
2022-02-05 19:54:31 -0700

    created routes to display the /login page and the /homepage page and fixed some styling/spacing

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,5 +41,56 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/homepage', (req, res) => {
+    console.log(req.session);
+
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // pass a single post object into the homepage template
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', {
+                posts
+                //loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+// router.get('/login', (req, res) => {
+//     if (req.session.loggedIn) {
+//         res.redirect('/');
+//         return;
+//     }
+
+//     res.render('login');
+// });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
 module.exports = router;
 
