a83acd2e17671fe5f3febd7e201f1c4ef4664c36
sm3131 <sm2683@nau.edu>
2022-02-06 17:44:56 -0700

    added functionality to render the edit post page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -42,4 +42,45 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/edit/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_text', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+            console.log(post.title);
+            // pass data to template
+            res.render('edit-post', {
+                post
+                //loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
