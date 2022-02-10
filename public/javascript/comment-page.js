3dbd937a8b4c0903b4f5c9ae3ebb50edafab386e
sm3131 <sm2683@nau.edu>
2022-02-10 03:00:42 -0700

    added functions to delete a comment

--- public/javascript/comment-page.js
+++ public/javascript/comment-page.js
@@ -1,26 +1,22 @@
+const buttons = document.querySelectorAll('.delete-comment');
+buttons.forEach(button => {
+    button.addEventListener('click', commentPageHandler);
+})
+
 async function commentPageHandler(event) {
     event.preventDefault();
 
-    const id = req.session.user_id;
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_text = document.querySelector('textarea[name="post-body"]').value;
-
-    const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            title,
-            post_text
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+    const id = event.target.name;
+   
+    const response = await fetch(`/api/comments/${id}`, {
+        method: 'DELETE'
     });
 
     if (response.ok) {
-        document.location.replace('/dashboard');
+        //document.location.replace('/dashboard');
+        document.location.reload();
     } else {
         alert(response.statusText);
     }
 }
 
-document.querySelector('.comment-btn').addEventListener('click', commentPageHandler);
